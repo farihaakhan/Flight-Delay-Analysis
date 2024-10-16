35940d4001d6bfa9c98cd16a00a9b9f74034e673
Fariha Khan <kfariha99@gmail.com>
2024-10-15 22:57:31 -0400

    Update flight_analysis.py

--- flight_analysis.py
+++ flight_analysis.py
@@ -8,7 +8,7 @@ import seaborn as sns  # Import seaborn for plotting
 df1 = pd.read_csv('flights2022.csv')
 df2 = pd.read_csv('flights_weather2022.csv')
 
-# Create new column, combining others
+# Create a new column, combining others
 df2['Route'] = df2['origin'] + '-' + df2['dest']
 
 
@@ -18,13 +18,7 @@ routes_delays_cancels = df2.groupby('Route').agg(
     high_cancelled_flights=('dep_time', lambda x: x.isnull().sum())
 ).reset_index()
 
-# Airline Delays
-airlines_delays_cancels = df2.groupby('airline').agg(
-    avg_dep_delay =('dep_delay', 'mean'),
-    high_cancelled_flights=('dep_time', lambda x: x.isnull().sum())
-).reset_index()
-
-# Create a bar graph for highest number of cancellations by Routes
+# Create a bar graph for the highest number of cancellations by Routes
 top9_route_cancels_bar = routes_delays_cancels.nlargest(9, 'high_cancelled_flights')
 sns.barplot(data=top9_route_cancels_bar, y='high_cancelled_flights', x='Route', palette='inferno', order=top9_route_cancels_bar['Route'])
 plt.title('Top 9 Routes with Highest Number of Cancellations')
@@ -34,13 +28,30 @@ plt.ylabel('Total Cancellations')
 plt.show()
 plt.close()
 
-# Create a bar graph for highest number of cancellations by Airlines
+# Airline Delays
+airlines_delays_cancels = df2.groupby('airline').agg(
+    avg_dep_delay =('dep_delay', 'mean'),
+    high_cancelled_flights=('dep_time', lambda x: x.isnull().sum())
+).reset_index()
+
+# Create a bar graph for the highest number of cancellations by Airlines
 top9_airline_cancels_bar = airlines_delays_cancels.nlargest(9, 'high_cancelled_flights')
 sns.barplot(data=top9_airline_cancels_bar, y='high_cancelled_flights', x='airline', palette='plasma', order=top9_airline_cancels_bar['airline'])
 plt.title('Top 9 Airline with Highest Number of Cancellations')
 plt.xlabel('Airline')
 plt.xticks(rotation=90,fontsize=8)
 plt.ylabel('Total Cancellations')
+plt.show()
+plt.close()
+
+# Create a bar graph for the highest number of delays by Airlines
+top9_airline_delays_bar = airlines_delays_cancels.nlargest(9, 'avg_dep_delay')
+
+sns.barplot(data=top9_airline_cancels_bar, y='avg_dep_delay', x='airline', palette='magma', order=top9_airline_cancels_bar['airline'])
+plt.title('Airline with Highest Average Departure Delay')
+plt.xlabel('Airline')
+plt.xticks(rotation=90,fontsize=8)
+plt.ylabel('Average Departure Delay')
 plt.show()
 plt.close()
 
